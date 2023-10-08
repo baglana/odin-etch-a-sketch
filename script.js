6308c7916a8a7931a165aa936f194b1f6ef806ed
baglana <102351104+baglana@users.noreply.github.com>
2023-10-08 14:39:34 +0600

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const container = document.querySelector('.container');
 
-function createGrid(size = 16) {
-  for (let i = 1; i <= size; i++) {
-    for (let j = 1; j <= size; j++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      
-      square.addEventListener('mouseenter', () => {
-        square.classList.add('hovered');
-      })
-      square.addEventListener('mouseleave', () => {
-        square.classList.remove('hovered');
-      })
-
-      container.appendChild(square);
+addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('#grid-size-btn');
+  btn.addEventListener('click', () => {
+    const selectedSize = prompt(
+      'Enter the number of squares per side for the new grid \
+      (values greater than 100 will be lowered to 100):',
+      16
+    );
+    if (selectedSize !== null) {
+      removeAllChildElements(container);
+      createGrid(+selectedSize);
     }
+  });
+});
+
+function removeAllChildElements(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function createGrid(size = 16) {
+  size = (size > 100) ? 100 : size;
+  for (let i = 1; i <= size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${100 / size}%`;
+    
+    square.addEventListener('mouseenter', () => {
+      square.classList.add('hovered');
+    })
+
+    container.appendChild(square);
   }
 }
 
