c6104dda0f4fb739f272bfa29463d4ab8f638a2b
baglana <102351104+baglana@users.noreply.github.com>
2023-10-08 13:07:17 +0600

    Fix grid size to be 16x16

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const container = document.querySelector('.container');
 
 function createGrid(size = 16) {
   for (let i = 0; i < size; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.textContent = i + 1; 
-    container.appendChild(square);
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.textContent = (i + 1) * (j + 1); 
+      container.appendChild(square);
+    }
   }
 }
 
